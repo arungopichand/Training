796dad8ac0dd49ce6c8081b39b8914475c8351d5
arungopichand <arungopichand98@gmail.com>
2025-07-03 23:33:14 -0400

    day 4 july 03: Added TypeScript demo files and variable demos

---
+++ Day3_july02/addition.js
@@ -0,0 +1,5 @@
+let a = prompt("Enter the first number:");
+let b = prompt("Enter the second number:");
+console.log(`Addition of ${a} & ${b} is ${parseInt(a) + parseInt(b)}`);
+//window.alert(`Addition of ${a} & ${b} is ${parseInt(a) + parseInt(b)}`); // is only valid in browser not in nodejs
+//document.writeln(`Addition of ${a} & ${b} is ${parseInt(a) + parseInt(b)}`); // is only valid in browser not in nodejs
